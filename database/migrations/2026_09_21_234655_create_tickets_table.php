024bf71a2c94a48d449a8d68e79388280d72118f
pedru224 <res0147939@aluno.firjansenaisesi.com.br>
2026-09-22 19:20:52 -0300

    Update database models and seeders

--- database/migrations/2026_09_21_234655_create_tickets_table.php
+++ database/migrations/2026_09_21_234655_create_tickets_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('department_id')->constrained('departments')->onDelete('cascade');
+            $table->string('title');
+            $table->string('requester_name');
+            $table->enum('priority', ['Baixa', 'Média', 'Alta', 'Urgente']);
+            $table->text('description');
+            $table->string('status')->default('Aberto');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('tickets');
--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Department;
 
 class DepartmentSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
     public function run(): void
     {
-        //
+        Department::create([
+            'name' => 'TI / Suporte',
+            'code' => 'TI'
+        ]);
+
+        Department::create([
+            'name' => 'Sistemas & Software',
+            'code' => 'SOFT'
+        ]);
+
+        Department::create([
+            'name' => 'Recursos Humanos',
+            'code' => 'RH'
+        ]);
+
+        Department::create([
+            'name' => 'Infraestrutura & Manutenção',
+            'code' => 'INFRA'
+        ]);
     }
 }
