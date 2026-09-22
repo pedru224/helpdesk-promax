d0322de7d0b153cc17f5fa602677b8e9de5d73af
Gustavo Paiva Laurindo <res0147016@aluno.firjansenaisesi.com.br>
2026-09-22 19:48:40 -0300

    fix: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Department;
 
-class DepartmentSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Department::create(['name' => 'TI / Suporte', 'code' => 'TI']);
-        Department::create(['name' => 'Sistemas & Software', 'code' => 'SOFT']);
-        Department::create(['name' => 'Recursos Humanos', 'code' => 'RH']);
-        Department::create(['name' => 'Infraestrutura & Manutenção', 'code' => 'INFRA']);
+        $this->call([
+            DepartmentSeeder::class,
+        ]);
     }
 }
